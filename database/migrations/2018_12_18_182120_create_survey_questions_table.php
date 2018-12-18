be096da649be0f24c7b99ead97e63747f1af4d8c
Sireler <sireler@gmail.com>
2018-12-18 21:34:21 +0300

    Created migration of the survey questions table

---
+++ database/migrations/2018_12_18_182120_create_survey_questions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSurveyQuestionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_questions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('type', 32);
+            $table->unsignedInteger('survey_id');
+            $table->foreign('survey_id')
+                ->references('id')->on('surveys')
+                ->onDeltete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_questions');
+    }
+}
