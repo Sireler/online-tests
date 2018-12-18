80b41c33a18a5d76d54eba3bf07847a760bf7eb6
Sireler <sireler@gmail.com>
2018-12-18 22:10:44 +0300

    Fillable fields fixed

--- app/Survey.php
+++ app/Survey.php
@@ -6,10 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Survey extends Model
 {
-    protected $fillable = ['title', 'user_id'];
+    protected $fillable = ['title'];
 
     public function questions()
     {
         return $this->hasMany('App\SurveyQuestion');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
     }
 }
