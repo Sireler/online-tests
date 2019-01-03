1a39602c4184c3ac9832f2ddb7ca668f196de101
Sireler <sireler@gmail.com>
2019-01-03 19:20:44 +0300

    Delete answer action

--- app/Http/Controllers/Api/AnswerController.php
+++ app/Http/Controllers/Api/AnswerController.php
@@ -4,11 +4,29 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Traits\Responses as JsonResponses;
 
 class AnswerController extends Controller
 {
+    use JsonResponses;
+
     public function __construct()
     {
         $this->middleware('auth:api');
+    }
+
+    /**
+     * Delete an answer
+     *
+     */
+    public function delete(Request $request, $id)
+    {
+        $user = $request->user();
+
+        if ($user->hasSurvey($id)) {
+            //todo
+        } else {
+            $this->forbiddenResponse();
+        }
     }
 }
