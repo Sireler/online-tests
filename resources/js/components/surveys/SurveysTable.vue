17a5f9eb8718c004adce710a1789361116c2ef21
Sireler <sireler@gmail.com>
2019-07-19 21:04:47 +0300

    Refactoring vue components

---
+++ resources/js/components/surveys/SurveysTable.vue
@@ -0,0 +1,72 @@
+<template>
+    <table class="table bg-white rounded">
+        <thead>
+        <tr class="table-thead text-uppercase">
+            <th>Title</th>
+            <th>Created</th>
+            <th>Responses</th>
+            <th>Actions</th>
+        </tr>
+        </thead>
+        <tbody>
+        <tr v-for="(survey, index) in surveys">
+            <td>{{ survey.title }}</td>
+            <td>{{ survey.created_at }}</td>
+            <td>0</td>
+            <td>
+                                <span class="oi oi-circle-x" title="Delete survey"
+                                      @click="deleteSurvey(survey.id, index)"></span>
+                <span class="oi oi-wrench" title="Edit survey"
+                      @click="editSurvey(survey.id)"></span>
+            </td>
+        </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+    name: "SurveysTable",
+    props: ['surveys'],
+    methods: {
+        deleteSurvey(id, index) {
+            this.$dialogs.confirm('Are you sure you want to delete a survey?', {
+                title: 'Delete survey', okLabel: 'Delete'
+            }).then(res => {
+                if (res.ok) {
+                    this.axios.delete(`/survey/delete/${id}`)
+                        .then((res) => {
+                            this.surveysTable.splice(index, 1);
+                            this.$toasted.show(res.data.message);
+                        })
+                        .catch((err) => {
+                            this.$toasted.show('Forbidden');
+                        });
+                }
+            });
+        },
+        editSurvey(id) {
+            this.$router.push({ path: `/tests/edit/${id}` });
+        }
+    }
+}
+</script>
+
+<style scoped>
+.oi {
+    font-size: 1.15em;
+}
+.oi-circle-x {
+    color: #ff0000;
+}
+.oi-wrench {
+    color: #29ab0f;
+}
+
+span {
+    cursor: pointer;
+    color: #5f2a62;
+    user-select: none;
+    margin-left: 10px;
+}
+</style>
--- resources/js/components/tests/Tests.vue
+++ resources/js/components/tests/Tests.vue
@@ -3,27 +3,7 @@
         <div class="row">
             <div class="col-md-8 offset-md-2">
                 <table v-if="surveysTable !== null && surveysTable.length > 0" class="table bg-white rounded">
-                    <thead>
-                        <tr class="table-thead">
-                            <th>TITLE</th>
-                            <th>CREATED</th>
-                            <th>RESPONSES</th>
-                            <th>MANAGE</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(survey, index) in surveysTable">
-                            <td>{{ survey.title }}</td>
-                            <td>{{ survey.created_at }}</td>
-                            <td>0</td>
-                            <td>
-                                <span class="oi oi-circle-x" title="Delete survey"
-                                      @click="deleteSurvey(survey.id, index)"></span>
-                                <span class="oi oi-wrench" title="Edit survey"
-                                      @click="editSurvey(survey.id)"></span>
-                            </td>
-                        </tr>
-                    </tbody>
+                    <SurveysTable :surveys="surveysTable"></SurveysTable>
                 </table>
             </div>
         </div>
@@ -46,7 +26,12 @@
 </template>
 
 <script>
+import SurveysTable from '../surveys/SurveysTable';
+
 export default {
+    components: {
+        SurveysTable
+    },
     data() {
         return {
             surveysTable: null
@@ -66,25 +51,6 @@ export default {
                     window.location.reload();
                 });
         },
-        deleteSurvey(id, index) {
-            this.$dialogs.confirm('Are you sure you want to delete a survey?', {
-                title: 'Delete survey', okLabel: 'Delete'
-            }).then(res => {
-                if (res.ok) {
-                    this.axios.delete(`/survey/delete/${id}`)
-                        .then((res) => {
-                            this.surveysTable.splice(index, 1);
-                            this.$toasted.show(res.data.message);
-                        })
-                        .catch((err) => {
-                            this.$toasted.show('Forbidden');
-                        });
-                }
-            });
-        },
-        editSurvey(id) {
-            this.$router.push({ path: `/tests/edit/${id}` });
-        }
     },
     mounted() {
         this.getUserSurveys();
@@ -93,20 +59,5 @@ export default {
 </script>
 
 <style scoped>
-.oi {
-    font-size: 1.15em;
-}
-.oi-circle-x {
-    color: #ff0000;
-}
-.oi-wrench {
-    color: #29ab0f;
-}
 
-span {
-    cursor: pointer;
-    color: #5f2a62;
-    user-select: none;
-    margin-left: 10px;
-}
 </style>
