d51894f80e46a05b3a8b296d2e352d3d1b1ec5c1
Sireler <sireler@gmail.com>
2019-07-21 15:09:46 +0300

    SurveyResponses component

--- resources/js/components/surveys/responses/SurveyResponses.vue
+++ resources/js/components/surveys/responses/SurveyResponses.vue
@@ -5,6 +5,20 @@
                 <div class="card">
                     <div class="card-body">
                         <h4>Responses</h4>
+
+                        <div class="questions mb-2" v-for="question in questions">
+                            <div class="question-title p-2 bg-info text-primary rounded mb-2">
+                                Question: {{ question.title }}
+                            </div>
+
+                            <div class="answers ml-4">
+                                <div class="answer-item" v-for="(answer,i) in question.answers">
+                                    Answer ({{ i+1 }}): {{ answer.text }}
+                                </div>
+                            </div>
+                        </div>
+
+
                     </div>
                 </div>
             </div>
@@ -15,11 +29,31 @@
 <script>
 export default {
     name: "SurveyResponses",
+    data() {
+        return {
+            questions: []
+        }
+    },
+    methods: {
+        getResponses() {
+            this.axios.get(`/survey/votes/${this.id}/responses`)
+                .then((res) => {
+                    this.questions = res.data.questions;
+                })
+                .catch((err) => {
+                    this.$toasted.show('Forbidden');
+                });
+        }
+    },
     computed: {
         id() {
             return this.$route.params.id;
         }
     },
+
+    mounted() {
+        this.getResponses();
+    }
 }
 </script>
 
